8acc7cd03401f658557b99caed8243299661aace
Matheus Alexandre <matheus8352@gmail.com>
2021-11-05 17:29:47 -0300

    DataTable integration

--- frontend/src/types/sales.ts
+++ frontend/src/types/sales.ts
@@ -1,10 +1,33 @@
-export type SaleSum =     {
-    sellerName: string,
-    saleSum: number
-}
+import { Seller } from "./seller";
+
+export type Sale = {
+  id: number;
+  visited: number;
+  deals: number;
+  amount: number;
+  date: string;
+  seller: Seller;
+};
+
+export type SalePage = {
+  content?: Sale[];
+  last: boolean;
+  totalElements: number;
+  totalpages: number;
+  size?: number;
+  number: number;
+  first: boolean;
+  numberOfElements?: number;
+  empty?: boolean;
+};
+
+export type SaleSum = {
+  sellerName: string;
+  saleSum: number;
+};
 
 export type SaleSuccess = {
-    sellerName: string,
-    visited: number,
-    deals: number
-}
+  sellerName: string;
+  visited: number;
+  deals: number;
+};
